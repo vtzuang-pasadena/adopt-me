393889d87cb81cb49554985cd2392c787b9ba934
V <vtzuang@gmail.com>
2023-03-15 01:39:58 -0700

    added SearchParams useBreedList Pet Results

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,18 @@
-import React from "react";
-import { createRoot } from "react-dom";
-
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed),
-  ]);
-};
+//v11
+import { createRoot } from "react-dom/client";
+import SearchParams from "./SearchParams.jsx";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      animal: "Dog",
-      name: "Luna",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      animal: "Bird",
-      name: "Pepper",
-      breed: "Cockatiel",
-    }),
-    React.createElement(Pet, {
-      animal: "Cat",
-      name: "Doink",
-      breed: "Mixed",
-    }),
-  ]);
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <SearchParams />
+    </div>
+  );
 };
 
 const container = document.getElementById("root");
 
 const root = createRoot(container);
 
-root.render(React.createElement(App));
+root.render(<App />);
